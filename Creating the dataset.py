fbbbb240b1012d60d9fa454b547650054c3a765e
giuliaevolvi <giuliaevolvi@gmail.com>
2024-10-11 19:00:24 +0200

    Add files via upload

---
+++ Creating the dataset.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello, world!")
+
+
+# In[3]:
+
+
+import instaloader)
+
+
+# In[11]:
+
+
+import instaloader
+
+
+# In[10]:
+
+
+get_ipython().system('pip install instaloader')
+
+
+# In[18]:
+
+
+import instaloader
+
+
+# In[20]:
+
+
+print("Hello, world!")
+
+
+# In[22]:
+
+
+import instaloader
+
+# Create an instance of Instaloader
+L = instaloader.Instaloader()
+
+# Optional: Log in to Instagram (needed for private accounts)
+# Replace with your Instagram username and password
+# L.login('mergeproject2023', 'ILoveJesus!')
+
+# Download profile data
+profile = 'womensordination'  # Replace with the Instagram username you want to download
+L.download_profile(profile, profile_pic_only=False)
+
+
+# In[2]:
+
+
+import instaloader
+
+# Create an instance of Instaloader
+L = instaloader.Instaloader()
+
+# Optional: Log in to Instagram (needed for private accounts)
+# Replace with your Instagram username and password
+# L.login('mergeproject2023', 'ILoveJesus!')
+
+# Download profile data
+profile = 'citizenGO'  # Replace with the Instagram username you want to download
+L.download_profile(profile, profile_pic_only=False)
+
+
+# In[ ]:
+
+
+
+
